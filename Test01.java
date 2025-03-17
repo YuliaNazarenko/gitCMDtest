61312b789d5a8a250ae10b014f0a0349f21c0fb2
YuliaNazarenko <yuliya.nazarenko.2023@gmail.com>
2025-03-17 23:30:22 +0100

    NewStart

--- Test01.java
+++ Test01.java
@@ -2,5 +2,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.printf("Hello");
         System.out.printf("World");
+        System.out.printf("Hello always");
     }
 }
